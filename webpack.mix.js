42fbbf856d8af44c8b3919406284d848b36dcc15
Azizul Hasan <azizulhasan.webappick@gmail.com>
2025-08-13 00:07:45 +0600

    AR-24

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
 
-// mix.js('src/dashboard/index.js', 'admin/js/build/ar-try-on-dashboard-ui.min.js').react();
+mix.js('src/dashboard/index.js', 'admin/js/build/ar-try-on-dashboard-ui.min.js').react();
 mix.js('src/metabox/index.js', 'admin/js/build/ar-try-on-metabox-ui.min.js').react();
 mix.js('admin/js/ar-try-on-media-library.js', 'admin/js/build/ar-try-on-media-library.min.js');
 mix.js('public/js/ar-vr-3d-model-try-on-public.js', 'public/js/ar-vr-3d-model-try-on-public-dist.js');
